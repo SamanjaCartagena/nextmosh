f0839fe50248ea4544df81848fdc75bed4b350f3
SamanjaCartagena <cart.samanja09@gmail.com>
2024-01-21 23:32:47 -0500

    practice finished

--- app/components/ProductCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AddToCart from './AddToCart'
-const ProductCard = () => {
-  return <div>
-    <AddToCart />
-      </div>;
-};
-
-export default ProductCard;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,18 +2,28 @@ import React from "react";
 interface User{
     id:number;
     name:string;
+    email:string;
 }
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users',
-  {next:{revalidate:10}})
+  )
   const users:User[]= await res.json()
   return (
     <>
     <h1>Users</h1>
     <p>{new Date().toLocaleTimeString()}</p>
-    <ul>
-        {users.map(user =><li key={user.id}>{user.name}</li> )}
-    </ul>
+    <table className='table table-bordered'>
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+                    {users.map(user =><tr key={user.id}><td>{user.name}</td><td>{user.email}</td></tr> )}
+
+        </tbody>
+    </table>
     </>
   );
 };
